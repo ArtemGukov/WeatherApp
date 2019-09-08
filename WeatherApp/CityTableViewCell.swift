3698da74c63a4331ed744e1453b90d66f373ba0a
Artem Gukov <artem.gukov@gmail.com>
2019-09-08 22:05:11 +0300

    First commit

---
+++ WeatherApp/CityTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CityTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Артем on 06/09/2019.
+//  Copyright © 2019 Gukov.tech. All rights reserved.
+//
+
+import UIKit
+
+class CityTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var regionLabel: UILabel!
+    
+    @IBOutlet weak var iconImage: UIImageView!
+}
---
+++ WeatherApp/DetailCityViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailCityViewController.swift
+//  WeatherApp
+//
+//  Created by Артем on 05/09/2019.
+//  Copyright © 2019 Gukov.tech. All rights reserved.
+//
+
+import UIKit
+
+class DetailCityViewController: UIViewController {
+    
+    @IBOutlet weak var segmentContolWeather: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    
+    @IBAction func segmentControlPressed(_ sender: UISegmentedControl) {
+    }
+}
